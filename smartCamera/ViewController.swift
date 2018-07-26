407f9d6e1a3c715a4a91ab53248ff1cfcfd5e1fc
Husnain <husnainali@novatoresols.com>
2018-07-26 16:56:27 +0500

    Initial Commit

---
+++ smartCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smartCamera
+//
+//  Created by Husnain on 26/07/2018.
+//  Copyright © 2018 Husnain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
